d493b6e8a353c502d7df956fcd293e71fd639668
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-11-21 14:06:24 +0545

    servers buggly

--- main.c
+++ main.c
@@ -20,20 +20,57 @@ char *recv_data(int client_fd)
 {
 	char *buffer = malloc(1024);
 
-	int n = read(client_fd, buffer, sizeof(buffer)-1);
+	if (!buffer)
+	{
+		printf("Memory Allocation failed:");
+		exit(1);
+	}
+
+	int n = read(client_fd, buffer, 1024-1);
 	buffer[n] = '\0';
 	return buffer;
+}
+
+int send_data(int client_fd, char *data,int len)
+{
+	char len_in_string[10] = "";
+	sprintf(len_in_string,"%d",len);
+	
+	char *response = malloc(strlen(len_in_string)+len+HTTP_RESPONSE_TEMPLATE_LEN+1); 
+	
+	if (!response)
+	{
+		printf("Memory Allocation Failed:");
+		exit(1);
+	}
+
+	response[0] = '\0'; // making this a string type
+	
+	
+	strcat(response,HTTP_RESPONSE_TEMPLATE_HEADER);
+	strcat(response,len_in_string);
+	strcat(response,HTTP_RESPONSE_TEMPLATE_FOOTER);
+	strcat(response,data);
+	
+	printf("%s",response);
+
+	send(client_fd,response,len+HTTP_RESPONSE_TEMPLATE_LEN,0); // TODO:HERE TOO
+
+	free(response);
+	return 1;
 }
 
 int main(){
 	int server_fd = create_server(INADDR_ANY,PORT);
 	int client_fd = accept(server_fd, NULL, NULL);
 
-	char *buffer = recv_data(client_fd);
-	
-	printf("RESPOSE: %s\n",buffer);
+	char *req = recv_data(client_fd);
 
-	free(buffer);	
+	
+	int result = send_data(client_fd,"hello from server",strlen("hello from server"));
+		
+
+	free(req);	
 	close(client_fd);
 	close(server_fd);
 }
